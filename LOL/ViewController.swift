81efa8c483c00b15f29dfc942cfa5e2df6bc7783
Diggo <rdiggosilva@gmail.com>
2024-03-11 13:10:13 -0300

    Starting App League of Legends. Application created for study purposes

---
+++ LOL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LOL
+//
+//  Created by Diggo Silva on 11/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
